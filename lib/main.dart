6acf08edfec9997a053105fba7b37baf078a119a
jaychang99 <jaychang99@gmail.com>
2024-02-03 17:26:56 +0900

    feat: Scaffold 를 이용

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello, World!'),
+      home: Scaffold(
+        body: Text('Hello World'),
+      ),
     );
   }
 }
