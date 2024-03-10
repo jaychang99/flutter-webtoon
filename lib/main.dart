3876bbef9c75fdd92652d402c40c6044b66c8117
jaychang99 <jaychang99@gmail.com>
2024-03-10 11:59:19 +0900

    refactor(4.3): MyLargeTitle 빼내기

--- lib/main.dart
+++ lib/main.dart
@@ -27,34 +27,33 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
+        backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              for (var number in numbers)
-                Text(
-                  '$number',
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(Icons.add_box_rounded),
-              )
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
       ),
     );
   }
