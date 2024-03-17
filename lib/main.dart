c3d7c25597d83dc56c6a1844e0f235dc6e099788
jaychang99 <jaychang99@gmail.com>
2024-03-17 19:48:12 +0900

    feat(5.0): 포모도로 앱 개발에 필요한 테마 색상 지정

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter'),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
         ),
-        body: Center(
-          child: Text('Hello World'),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const Scaffold(
+        body: Center(),
       ),
     );
   }
